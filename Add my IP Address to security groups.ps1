be35e54fae2d81c78b87b85645136b952d9be4e6
unknown <Ankit@DW0HR72.graphicint.com>
2021-01-24 11:33:54 +0400

    Added basic instaructions

--- Add my IP Address to security groups.ps1
+++ Add my IP Address to security groups.ps1
@@ -1,3 +1,6 @@
+#Install AWS cli and configure it
+#create a startup script, Win + r --> shell:startup and add a shortcut to the powershell script which runs everytime the computer starts
+
 $ip=(Invoke-WebRequest -uri "http://ipconfig.me/ip" -usebasicparsing).Content+"/32" 
 aws ec2 authorize-security-group-ingress --group-name pihole --protocol tcp --port 80 --cidr $ip --region ap-south-1
 aws ec2 authorize-security-group-ingress --group-name default --protocol tcp --port 3389 --cidr $ip --region ap-south-1
